ec1eb89a1ae3ca1aa47ec53c900a4bd1d26e9c37
Junwei <1148131355@qq.com>
2024-02-11 10:33:11 -0500

    hw0

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,65 +1,105 @@
+#include <stdexcept>
+
 #include "byte_stream.hh"
 
 using namespace std;
 
 ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
-bool Writer::is_closed() const
-{
-  // Your code here.
-  return {};
-}
-
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
-  return;
+  
+  
+
+  for(uint64_t i = 0; i < data.length(); i++){
+    if(byteQueue.size() == capacity_){
+      break;
+    }
+    byteQueue.push(data[i]);
+    total_pushed ++;
+  }
+
+
 }
 
 void Writer::close()
 {
   // Your code here.
+  is_wr_closed_ = true;
+}
+
+void Writer::set_error()
+{
+  // Your code here.
+  is_wr_error_ = true;
+}
+
+bool Writer::is_closed() const
+{
+  // Your code here.
+  return is_wr_closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return capacity_ - byteQueue.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
-}
-
-bool Reader::is_finished() const
-{
-  // Your code here.
-  return {};
-}
-
-uint64_t Reader::bytes_popped() const
-{
-  // Your code here.
-  return {};
+  return total_pushed;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  if(byteQueue.size() == 0){
+    return std::string_view("");;
+  }
+
+  // unsigned char peekByte = byteQueue.front();
+  std::string_view byteStringView(reinterpret_cast<const char*>(&byteQueue.front()), 1);
+  return byteStringView;
+}
+
+bool Reader::is_finished() const
+{
+  // Your code here.
+  return is_wr_closed_ && byteQueue.size() == 0;
+}
+
+bool Reader::has_error() const
+{
+  // Your code here.
+  return is_wr_error_;
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  
+  for(uint64_t i = 0; i < len; i++){
+    if(byteQueue.size() == 0){
+      break;
+    }
+    byteQueue.pop();
+    total_poped++;
+  }
+
+ 
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return byteQueue.size();
+}
+
+uint64_t Reader::bytes_popped() const
+{
+  // Your code here.
+  return total_poped;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <cstdint>
+#include <queue>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
@@ -9,6 +10,15 @@ class Writer;
 
 class ByteStream
 {
+protected:
+  uint64_t capacity_;
+  // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
+  std::queue<unsigned char> byteQueue;
+  bool is_wr_closed_ = false;
+  bool is_wr_error_ = false;
+  uint64_t total_pushed = 0;
+  uint64_t total_poped = 0;
+
 public:
   explicit ByteStream( uint64_t capacity );
 
@@ -17,21 +27,15 @@ public:
   const Reader& reader() const;
   Writer& writer();
   const Writer& writer() const;
-
-  void set_error() { error_ = true; };       // Signal that the stream suffered an error.
-  bool has_error() const { return error_; }; // Has the stream had an error?
-
-protected:
-  // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
-  uint64_t capacity_;
-  bool error_ {};
 };
 
 class Writer : public ByteStream
 {
 public:
   void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
-  void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
+
+  void close();     // Signal that the stream has reached its ending. Nothing more will be written.
+  void set_error(); // Signal that the stream suffered an error.
 
   bool is_closed() const;              // Has the stream been closed?
   uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
@@ -44,7 +48,9 @@ public:
   std::string_view peek() const; // Peek at the next bytes in the buffer
   void pop( uint64_t len );      // Remove `len` bytes from the buffer
 
-  bool is_finished() const;        // Is the stream finished (closed and fully popped)?
+  bool is_finished() const; // Is the stream finished (closed and fully popped)?
+  bool has_error() const;   // Has the stream had an error?
+
   uint64_t bytes_buffered() const; // Number of bytes currently buffered (pushed and not popped)
   uint64_t bytes_popped() const;   // Total number of bytes cumulatively popped from stream
 };
