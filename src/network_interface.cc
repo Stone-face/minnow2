63efa2559c8baafdb45cf919d63fbde9c49c77fb
Junwei <1148131355@qq.com>
2024-03-16 18:08:41 -0400

    hw6

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -43,7 +43,7 @@ void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Addre
   // dgram.serialize( s );
   // frame.payload = std::move(s.output());
   
-  frame.payload = srd::move(serialize(dgram));
+  frame.payload = std::move(serialize(dgram));
   for (size_t i = 0; i < frame.payload.size(); i++) {
     std::cout << "serialized value: " << frame.payload[i] << endl;
   }
