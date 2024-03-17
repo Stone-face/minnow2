6b5055b99aff57970508375cc5651db5e1e95ce6
Junwei <1148131355@qq.com>
2024-03-16 20:07:00 -0400

    hw6

--- src/router.cc
+++ src/router.cc
@@ -69,6 +69,7 @@ void Router::route()
     
     while (!que.empty()) {
       InternetDatagram datagram = que.front();  
+      que.pop(); 
       int maxLen = -1;
       int maxInd = -1;
 
@@ -105,7 +106,7 @@ void Router::route()
         }
       }
 
-      que.pop(); 
+      // que.pop(); 
     }
   }
 }
