3537670825f242ad4fb18ebcfc81619dbd95a86f
Junwei <1148131355@qq.com>
2024-03-26 17:11:36 -0400

    up

--- apps/bidirectional_stream_copy.hh
+++ apps/bidirectional_stream_copy.hh
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../util/socket.hh"
+#include "socket.hh"
 
 //! Copy socket input/output to stdin/stdout until finished
 void bidirectional_stream_copy( Socket& socket, std::string_view peer_name );
